a571f383ddb788d9e9b48e6dac606cf2e9539af3
Joseph Baca <josephcbaca@gmail.com>
2020-09-14 20:35:11 -0700

    Update code to start towards saving book data to database and button to save and navigate to library.

--- client/src/components/pages/Search/index.js
+++ client/src/components/pages/Search/index.js
@@ -9,7 +9,7 @@ function Search({ loggedIn, currentUser }) {
   const [result, setResult] = useState([]);
   const [isSelected, setIsSelected] = useState("")
 
-  // Posts search to api route
+  // GET search to api route
   // Otherwise we log any errors
   function searchBooks(e) {
     e.preventDefault();
@@ -32,6 +32,32 @@ function Search({ loggedIn, currentUser }) {
       })
   }
 
+  // Posts a new hosted game to database
+  // Otherwise we log any errors
+  function addNewBook(e) {
+    e.preventDefault();
+    const bodyObj = {
+      title: "",
+      author: "",
+      synopsis: "",
+      ReaderId: ""
+    }
+    console.log(bodyObj);
+    axios.post("/api/host-new-game", bodyObj)
+      .then(res => {
+        console.log(res);
+        if (!res.data.errmsg) {
+          console.log("success!");
+        } else {
+          console.log("ERR");
+        }
+      })
+      .catch(err => {
+        console.log("Create Game errored.  Try again.");
+        console.log(err);
+      })
+  }
+
   const handleBookChange = e => {
     setIsSelected(e.target.name);
     console.log(e.target.name)
@@ -42,22 +68,29 @@ function Search({ loggedIn, currentUser }) {
   return (<div>
     <Navbar />
     <div className="container">
-      <div className="row">
-        <div className="col-4">
-          <input
-            className="form-control"
-            type="text"
-            placeholder="Search Books"
-            name="searchBooks"
-            onChange={e => setBooks(e.target.value)}
-          />
-          <button onClick={searchBooks} type="button" className="btn btn-outline-dark mb-3">Search</button>
-        </div>
+      <div className="row mb-3">
+        <input
+          className="form-control col-4"
+          type="text"
+          placeholder="Search Books"
+          name="searchBooks"
+          onChange={e => setBooks(e.target.value)}
+        />
+      </div>
+      <div className="row mb-3">
+        <button onClick={searchBooks} type="button" className="btn btn-outline-dark col-4">Search</button>
+
+        <Link className="col-4" to="/library"><button type="button" className="btn btn-outline-dark col-12">My Library</button></Link>
+        {/* { //Check if message failed
+          (result.length < 0)
+        ? <div></div>
+        : <button onClick={addNewBook} type="button" className="btn btn-outline-dark col-4">Add to Shelf</button>
+        } */}
       </div>
       <div className="row">
-        { //Check if message failed
+        {
           (result.length < 0)
-            ? <div> Search for books!</div>
+            ? <p>Search for books!</p>
             : result.map(book => <div className="row" key={book.id}>
               <input onChange={handleBookChange} name={book.volumeInfo.title} checked={isSelected === book.volumeInfo.title} type="checkbox" class="form-check-input time-dropbox" id="exampleCheck1"></input>
               <img className="col-2" src={book.volumeInfo.imageLinks.thumbnail}></img>
